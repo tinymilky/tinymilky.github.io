e8bc8f9f3ca0d044dbf48064e2a79328529ac879
Hang Zhang <scuzhanghang@hotmail.com>
2021-03-08 19:14:34 -0500

    update

--- demos/simple_cnn/convnetjs_mnist/cnn.js
+++ demos/simple_cnn/convnetjs_mnist/cnn.js
@@ -245,19 +245,20 @@ var visualize_activations = function(net, elt) {
                 // filters_div.appendChild(document.createTextNode('B-template values:'));
                 for (var j = 0; j < L.filters.length; j++) {
                     //display kernel values
-                    A = L.filters[j];
-                    for (var d = 0; d < A.depth; d++) {
-                        // filters_div.appendChild(document.createElement('br'));
-                        filters_div.appendChild(document.createTextNode('('));
-                        for (var x = 0; x < A.sx; x++) {
-                            for (var y = 0; y < A.sy; y++) {
-                                // filters_div.appendChild(document.createTextNode(Math.round(A.get(x,y,d) * 100) / 100 ));
-                                // filters_div.appendChild(document.createTextNode(' '));
-                            }
-                        }
-                        filters_div.appendChild(document.createTextNode(')'));
-                    }
+                    // A = L.filters[j];
+                    // for (var d = 0; d < A.depth; d++) {
+                    //     // filters_div.appendChild(document.createElement('br'));
+                    //     filters_div.appendChild(document.createTextNode('('));
+                    //     for (var x = 0; x < A.sx; x++) {
+                    //         for (var y = 0; y < A.sy; y++) {
+                    //             // filters_div.appendChild(document.createTextNode(Math.round(A.get(x,y,d) * 100) / 100 ));
+                    //             // filters_div.appendChild(document.createTextNode(' '));
+                    //         }
+                    //     }
+                    // }
+                    filters_div.appendChild(document.createTextNode('('));
                     draw_activations(filters_div, L.filters[j], 2);
+                    filters_div.appendChild(document.createTextNode(')'));
                 }
                 // gradients
                 filters_div.appendChild(document.createElement('br'));
